7feec8e17d07d73899b8174c936929375022b2e7
Devansh Sabharwal <sabharwaldevansh04@gmail.com>
2025-11-05 18:02:00 +0530

    task1 done

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,5 +1,61 @@
+import { ArrowUpRight } from "lucide-react";
 import React from "react";
 
-export default function Card() {
-  return <div></div>;
+interface Props {
+  heading: string;
+  subheading: string;
+  img: string;
+  onClick: () => void;
+  background: string;
+  buttonText: string;
+  direction: string;
+  buttonColor: string;
+}
+
+export default function Card(props: Props) {
+  const left = props.direction === "left";
+
+  return (
+    <div
+      className={`rounded-[50px] flex flex-col md:flex-row border border-black/10 h-auto md:h-[350px] overflow-hidden ${
+        left ? "md:flex-row-reverse" : ""
+      }`}
+      style={{ backgroundColor: props.background }}
+    >
+      <div className="w-full md:w-auto">
+        <img
+          className={`h-[250px] md:h-full w-full object-cover ${
+            !left
+              ? "md:rounded-bl-[50px] md:rounded-tl-[50px]"
+              : "md:rounded-br-[50px] md:rounded-tr-[50px]"
+          }`}
+          src={props.img}
+          alt="Fashion illustration"
+        />
+      </div>
+
+      <div className="flex-1 px-6 md:px-8 py-6 md:py-4 flex flex-col justify-center">
+        <div className="font-anton text-4xl sm:text-5xl md:text-[64px] leading-tight">
+          {props.heading}
+        </div>
+        <div className="mt-3 text-base sm:text-lg font-inter">
+          {props.subheading}
+        </div>
+        <div className="mt-8 md:mt-16">
+          <button
+            // onClick={props.onClick}
+            className="active:scale-95 cursor-pointer hover:scale-[105%] transition-all duration-300 flex items-center gap-3 md:gap-4  px-5 md:px-6 py-3 md:py-4 rounded-3xl md:rounded-4xl text-white"
+            style={{ backgroundColor: props.buttonColor }}
+          >
+            <span className="text-sm sm:text-base md:text-lg font-medium font-inter tracking-tighter">
+              {props.buttonText}
+            </span>
+            <span className="p-1 rounded-full border">
+              <ArrowUpRight size={18} />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function Page() {
   return (
     <div className="my-2 sm:my-4 mx-4 sm:mx-12 lg:mx-20">
       <Navbar />
       <Hero />
+      <div className="mt-12 flex flex-col gap-4">
+        <Card
+          background="#fff"
+          heading="Need a Designer?"
+          subheading="Discover talented designers, explore unique styles, and turn your ideas into reality"
+          img="/designers-group-1.png"
+          onClick={() => {}}
+          buttonText="Find Now"
+          direction="left"
+          buttonColor="#6F4E37"
+        />
+        <Card
+          background="#EBEBE9"
+          heading="Signup as Designer"
+          subheading="Showcase your portfolio, connect with clients, and bring your creative vision to life"
+          img="/image.png"
+          onClick={() => {}}
+          buttonText="Join Now"
+          direction="right"
+          buttonColor="black"
+        />
+      </div>
     </div>
   );
 }
